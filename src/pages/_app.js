7033aa3813610030dd32d20d0496a0ff08819589
unknown <Jorgemathez14@gmail.com>
2024-09-28 13:26:54 -0300

    NavBar , Home

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
+
+import Footer from '@/Components/Footer';
 import '@/styles/globals.css'
+import { ThemeProvider } from "@material-tailwind/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+      <Footer/>
+    </ThemeProvider>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,18 @@
+"use client";
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import NavBar from '@/Components/NavBar'
+import Home from '@/Components/Home'
 
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function index() {
   return (
  
 <div> 
 <NavBar/>
-  Este es mi Home
+<Home/>
 </div>
   )
 }
